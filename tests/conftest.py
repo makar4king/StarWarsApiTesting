b1badaef7336bd2ebc7f061d625726e091aaddb6
ma.panchekhin <ma.panchekhin@2Gis.ru>
2021-01-21 23:51:00 +0700

    upgrade for my test suite

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,33 +2,50 @@ import pytest
 import requests
 
 
+# A fixture that returns a response to a GET request
+@pytest.fixture()
+def get_request():
+    def request(url):
+        response = requests.get(url)
+        return response
+
+    return request
+
+
+# A fixture that returns a response to a GET request, but converted in JSON format
+@pytest.fixture()
+def get_request_and_convert_to_json():
+    def request(url):
+        response = requests.get(url).json()
+        return response
+
+    return request
+
+
 # Fixture which returns list of all people
 @pytest.fixture()
-def return_all_people():
-    number_of_page = 1
+def get_all_people(get_request_and_convert_to_json):
     results = []
-    next_link = 'not_null'
+    next_link = 'https://swapi.dev/api/people/?page=1'
     while next_link is not None:
-        response = requests.get(f"https://swapi.dev/api/people/?page={number_of_page}")
-        people = response.json()
+        people = get_request_and_convert_to_json(next_link)
         results += people['results']
         next_link = people['next']
-        number_of_page += 1
     return results
 
 
 # Fixture which returns json-schema of people object
 @pytest.fixture()
-def json_schema_of_people():
-    schema = requests.get('https://swapi.dev/api/people/schema').json()
+def json_schema_of_people(get_request_and_convert_to_json):
+    schema = get_request_and_convert_to_json('https://swapi.dev/api/people/schema')
     return schema
 
 
 # Fixture which returns search people results
 @pytest.fixture()
-def return_search_people_results():
+def return_search_people_results(get_request_and_convert_to_json):
     def request(query):
-        response = requests.get(f"https://swapi.dev/api/people/?search={query}").json()
+        response = get_request_and_convert_to_json(f"https://swapi.dev/api/people/?search={query}")
         return response
 
     return request
--- tests/test_swapi.py
+++ tests/test_swapi.py
@@ -1,4 +1,3 @@
-import requests
 import pytest
 from jsonschema import validate
 from test_data import *
@@ -7,15 +6,14 @@ from test_data import *
 class TestStarWarsApi:
 
     # Test which checks what length of people's list is equal to field 'count'
-    def test_check_count_of_all_people(self, return_all_people):
-        data = return_all_people
-        response = requests.get('https://swapi.dev/api/people/')
-        people = response.json()
+    def test_check_count_of_all_people(self, get_all_people, get_request_and_convert_to_json):
+        data = get_all_people
+        people = get_request_and_convert_to_json('https://swapi.dev/api/people/')
         assert len(data) == people['count'], "Length of array is not equal to field 'count'"
 
     # Test which checks that all people names are unique
-    def test_unique_names(self, return_all_people):
-        data = return_all_people
+    def test_unique_names(self, get_all_people):
+        data = get_all_people
         names_list = []
         for i in range(len(data)):
             names_list.append(data[i]['name'])
@@ -24,41 +22,40 @@ class TestStarWarsApi:
             assert names_list[i] != names_list[i - 1], "There is same names"
 
     # Test which checks there are no page with number 0 for people requests
-    def test_no_number_zero_page(self):
-        response = requests.get('https://swapi.dev/api/people/?page=0')
+    def test_no_number_zero_page(self, get_request):
+        response = get_request('https://swapi.dev/api/people/?page=0')
         assert response.status_code == 404
 
     # Parametrized test which checks there are 3 Skywalker's, 1 Vader, 2 Darth's
     @pytest.mark.parametrize('match,equal', search_names_test_data)
-    def test_search_names(self, match, equal):
-        response = requests.get(f"https://swapi.dev/api/people/?search={match}")
-        people = response.json()
+    def test_search_names(self, match, equal, get_request_and_convert_to_json):
+        people = get_request_and_convert_to_json(f"https://swapi.dev/api/people/?search={match}")
         assert people['count'] == equal
 
     # Test which validates that all people objects contain required schema fields
-    def test_validate_people_objects(self, return_all_people, json_schema_of_people):
-        for i in range(len(return_all_people)):
-            assert validate(return_all_people[i], schema=json_schema_of_people) is None
+    def test_validate_people_objects(self, get_all_people, json_schema_of_people):
+        for i in range(len(get_all_people)):
+            assert validate(get_all_people[i], schema=json_schema_of_people) is None
 
     # Test which validates that search for people is case insensitive
-    def test_check_search_insensitive_to_upper_case(self):
-        response_base = requests.get(f"https://swapi.dev/api/people/1").json()
+    def test_check_search_insensitive_to_upper_case(self, get_request_and_convert_to_json):
+        response_base = get_request_and_convert_to_json(f"https://swapi.dev/api/people/1")
         name_upper_case = response_base['name'].upper()
-        response_by_upper = requests.get(f"https://swapi.dev/api/people/?search={name_upper_case}").json()
+        response_by_upper = get_request_and_convert_to_json(f"https://swapi.dev/api/people/?search={name_upper_case}")
         assert response_base['name'] == response_by_upper['results'][0]['name']
 
     # Test which validates that search for people is case insensitive
-    def test_check_search_insensitive_to_lower_case(self):
-        response_base = requests.get(f"https://swapi.dev/api/people/1").json()
+    def test_check_search_insensitive_to_lower_case(self, get_request_and_convert_to_json):
+        response_base = get_request_and_convert_to_json(f"https://swapi.dev/api/people/1")
         name_lower_case = response_base['name'].upper()
-        response_by_lower = requests.get(f"https://swapi.dev/api/people/?search={name_lower_case}").json()
+        response_by_lower = get_request_and_convert_to_json(f"https://swapi.dev/api/people/?search={name_lower_case}")
         assert response_base['name'] == response_by_lower['results'][0]['name']
 
     # Test which validates that search for people is case insensitive
-    def test_check_search_insensitive_to_swap_case(self):
-        response_base = requests.get(f"https://swapi.dev/api/people/1").json()
+    def test_check_search_insensitive_to_swap_case(self, get_request_and_convert_to_json):
+        response_base = get_request_and_convert_to_json(f"https://swapi.dev/api/people/1")
         name_swap_case = response_base['name'].swapcase()
-        response_by_swap = requests.get(f"https://swapi.dev/api/people/?search={name_swap_case}").json()
+        response_by_swap = get_request_and_convert_to_json(f"https://swapi.dev/api/people/?search={name_swap_case}")
         assert response_base['name'] == response_by_swap['results'][0]['name']
 
     # Test which check that search for any char in English alphabet or any number from 0
@@ -66,3 +63,12 @@ class TestStarWarsApi:
     @pytest.mark.parametrize('query', check_search_result_test_data)
     def test_check_search_result(self, query, return_search_people_results):
         assert return_search_people_results(query)['count'] > 0
+
+    # A test that checks that the ID of the object in the list and in the url match
+    def test_check_ID_in_the_list_and_in_the_url_match(self, get_all_people):
+        data = get_all_people
+        list_of_urls = []
+        for i in range(len(data)):
+            list_of_urls.append(data[i]['url'])
+        for i in range(1, len(list_of_urls)):
+            assert list_of_urls[i-1] == f"http://swapi.dev/api/people/{i}/", f'Error in object #{i}'
